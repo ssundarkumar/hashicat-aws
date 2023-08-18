a29e0daa5c1fce513c9b6b14b1373ec94e9cf866
ssundarkumar <ssundarkumar@github.com>
2023-08-18 00:37:31 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -126,17 +126,13 @@ resource "aws_instance" "hashicat" {
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
-  tags = [
-	{
-    Name = "${var.prefix}-hashicat-instance",
-	},
-	{
-	key = "Department"
-	value = "devops"
-	}
-	]
-  
+  tags = {
+    Name = "${var.prefix}-hashicat-instance"
+    Department = "devops"
+  }
 }
+
+
 
 # We're using a little trick here so we can run the provisioner without
 # destroying the VM. Do not do this in production.
