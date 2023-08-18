d4ae4ca0cd9c4629e2adf78760eca0de560763ee
ssundarkumar <ssundarkumar@github.com>
2023-08-18 00:29:52 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -126,9 +126,16 @@ resource "aws_instance" "hashicat" {
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
-  tags = {
-    Name = "${var.prefix}-hashicat-instance"
-  }
+  tags = [
+	{
+    Name = "${var.prefix}-hashicat-instance",
+	},
+	{
+	key = "Department"
+	value = "devops"
+	}
+	]
+  
 }
 
 # We're using a little trick here so we can run the provisioner without
